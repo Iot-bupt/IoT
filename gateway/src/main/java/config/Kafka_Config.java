06d709540196b55fe03d521369e23d30ac26b108
tangjialiang <phx108@sina.com>
2017-11-23 16:43:56 +0800

    Des: update the address Update:      Config      Kafka_Config

--- gateway/src/main/java/config/Kafka_Config.java
+++ gateway/src/main/java/config/Kafka_Config.java
@@ -5,7 +5,7 @@ package config;
  */
 public class Kafka_Config {
     public static String bootstrap_servers = "10.108.219.61:9092";
-    public static String metadata_broker_list = "0.108.219.61:9092" ;
+    public static String metadata_broker_list = "10.108.219.61:9092" ;
     public static String key_serializer = "org.apache.kafka.common.serialization.StringSerializer" ;
     public static String value_serializer = "org.apache.kafka.common.serialization.StringSerializer" ;
     public static String recorder_topic = "data" ;
